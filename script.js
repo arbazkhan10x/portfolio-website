1bbda6e6d0bd112f2f8ede32993d01c4cc2ab507
arbazkhan10x <arbazkhan10x@gmail.com>
2026-03-17 17:30:13 +0530

    Add Navbar scroll effect

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ function changeText() {
         index = 0;
     }
 }
+
+window.addEventListener("scroll", function() {
+    let nav = document.querySelector("nav");
+
+    if(window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
