800ed908acb32d2e2dbe5bc87d1d48e495090e2b
arbazkhan10x <arbazkhan10x@gmail.com>
2026-04-04 13:20:39 +0530

    Portfolio website done

--- script.js
+++ script.js
@@ -4,47 +4,6 @@ function changeText() {
     "I am learning JavaScript 🚀";
 }
 
-// let messages = [
-//     "I am learning JavaScript",
-//     "Building my future with code",
-//     "Future Full Stack Developer"
-// ];
-
-// let index = 0;
-
-// function changeText() {
-//     document.getElementById("hero-text").innerText = messages[index];
-//     index++;
-
-//     if (index === messages.length) {
-//         index = 0;
-//     }
-// }
-
-
-// setInterval(changeText, 2000);
-
-
-// window.addEventListener("scroll", function() {
-//     let nav = document.querySelector("nav");
-
-//     if(window.scrollY > 50) {
-//         nav.classList.add("scrolled");
-//     } else {
-//         nav.classList.remove("scrolled");
-//     }
-// });
-
-//     document.querySelector(".contact-form").addEventListener("subbmit", function(e) {
-//         e.preventDefault();
-
-//         alert("Message sent successfully");
-
-//         this.reset();
-//     })
-
-
-
 
 // ===== HERO TEXT AUTO CHANGE =====
 let messages = [
